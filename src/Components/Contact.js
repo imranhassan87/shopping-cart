307f9a74e47a11c7599eb385556117cbc270fc6a
imranhassan96 <imranhassan96>
2020-06-17 18:26:23 +0500

    animation added

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import { FiGithub } from "react-icons/fi";
 
 const Contact = () => {
     return (
         <div className="contact">
-            <a rel="noopener noreferrer" target="_blank" href='https://github.com/imranhassan87' >Github</a>
+            <a rel="noopener noreferrer" target="_blank" href='https://github.com/imranhassan87' ><FiGithub /> Github</a>
         </div>
     )
 }
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,7 +1,25 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Link } from 'react-router-dom'
+import gsap from 'gsap'
 
 const Header = () => {
+    useEffect(() => {
+        const tl = gsap.timeline()
+        tl.from('.header', 1.8, {
+            y: -200,
+            ease: 'power4.out',
+            stagger: {
+                amount: 0.3
+            }
+        })
+        tl.from('.link', 1.2, {
+            x: 280,
+            ease: 'Power3.easeOut',
+            stagger: {
+                amount: 0.3
+            },
+        })
+    }, [])
     return (
         <nav className="header">
             <div className="container row space-between v-center">
@@ -11,9 +29,9 @@ const Header = () => {
                 </div>
                 <div className="nav-routes">
                     <ul className="row v-center">
-                        <Link to='/'>Home</Link>
-                        <Link to='/cart'>Cart</Link>
-                        <Link to='/contact'>Contact</Link>
+                        <Link className="link" to='/'>Home</Link>
+                        <Link className="link" to='/cart'>Cart</Link>
+                        <Link className="link" to='/contact'>Contact</Link>
                     </ul>
                 </div>
             </div>
